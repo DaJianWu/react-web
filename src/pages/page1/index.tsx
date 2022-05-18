6b4a58d17359cf58bcd84716a1b395fb23b1158c
DaJianWu <wdj.destiny@gmail.com>
2022-05-18 22:18:14 +0800

    feat: 当页面输入一个URL后发生了什么？

--- src/pages/page1/index.tsx
+++ src/pages/page1/index.tsx
@@ -6,6 +6,7 @@ export const Page1: React.FC = () => {
   return (
     <div className={style.page}>
       <h1>page1</h1>
+      <a href='https://juejin.cn/post/7098998314799988749'>当页面输入一个URL后发生了什么？</a>
     </div>
   );
 };
